4c8d7923f9205fe39da67bcc609cd2b372655f67
Andrew Steinheiser <me@iamandrew.io>
2021-11-09 13:38:03 -0700

    call features from main bot file explicitly

--- bot.js
+++ bot.js
@@ -10,6 +10,9 @@ const { Botkit } = require('botkit');
 const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack');
 // Import mongodb storage adapter for botkit
 const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+// Import features for the lunch bot
+const { oauthRoutes, getTokenForTeam, getBotUserByTeam } = require('./features/routes_oauth');
+const { slackFeatures } = require('./features/slack_features');
 
 require('dotenv').config();
 
@@ -54,61 +57,9 @@ const controller = new Botkit({
   storage
 });
 
-// Once the bot has booted up its internal services, you can use them to do stuff.
-controller.ready(() => {
-  // load traditional developer-created local custom feature modules
-  controller.loadModules(__dirname + '/features');
-});
-
 controller.webserver.get('/', (req, res) => {
-  res.send(`This app is running Botkit ${ controller.version }.`);
+  res.send(`This app is running Botkit ${controller.version}.`);
 });
 
-controller.webserver.get('/install', (req, res) => {
-  // getInstallLink points to slack's oauth endpoint and includes clientId and scopes
-  res.redirect(controller.adapter.getInstallLink());
-});
-
-controller.webserver.get('/install/auth', async (req, res) => {
-  try {
-    const results = await controller.adapter.validateOauthCode(req.query.code);
-    console.log('FULL OAUTH DETAILS', results);
-
-    // Store token by team in bot state.
-    tokenCache[results.team_id] = results.bot.bot_access_token;
-    // Capture team to bot id
-    userCache[results.team_id] = results.bot.bot_user_id;
-
-    res.json('Success! Bot installed.');
-  } catch (err) {
-    console.error('OAUTH ERROR:', err);
-    res.status(401);
-    res.send(err.message);
-  }
-});
-
-let tokenCache = {};
-let userCache = {};
-
-if (process.env.TOKENS) tokenCache = JSON.parse(process.env.TOKENS);
-if (process.env.USERS) userCache = JSON.parse(process.env.USERS);
-
-async function getTokenForTeam(teamId) {
-  if (tokenCache[teamId]) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(tokenCache[teamId]), 150);
-    });
-  } else {
-    console.error('Team not found in tokenCache: ', teamId);
-  }
-}
-
-async function getBotUserByTeam(teamId) {
-  if (userCache[teamId]) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(userCache[teamId]), 150);
-    });
-  } else {
-    console.error('Team not found in userCache: ', teamId);
-  }
-}
+oauthRoutes(controller);
+slackFeatures(controller);
